2d86de71bbe4238927c6a68263aff0d2b49475db
Chandler Talbot <50780917+crtalbot98@users.noreply.github.com>
2022-06-26 16:37:52 -0400

    Add route to buy a single cola (#3)

---
+++ backend/src/routes/buyOne.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import ColaModel from '../models/colaModel.js';
+
+const router = Router();
+
+export default router.get('/buyOne/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try{
+    const cola = await ColaModel.findById(id);
+
+    if(cola.num_available < 1) throw new Error(`${cola.name} is out of stock`);
+
+    cola.num_available -= 1;
+    cola.save();
+
+    res.send(cola)
+  }
+  catch(err) {
+    console.log('ERROR :: colas getOneById', err);
+    res.status(500).send({ Error: err.message })
+  }
+})
