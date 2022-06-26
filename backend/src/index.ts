caf4498bdf5a21d3e6ab11b3976a3edfb7aa4a19
Chandler Talbot <crtalbot98@gmail.com>
2022-06-26 16:37:30 -0400

    Add route to buy a single cola

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import getAll from './routes/getAll.js';
 import updateOne from './routes/updateOne.js';
+import buyOne from './routes/buyOne.js';
 import initDB from './db.js';
 import cors from 'cors';
 
@@ -18,6 +19,7 @@ app.use(cors({
 app.use(express.json());
 app.use('/cola', getAll);
 app.use('/cola', updateOne);
+app.use('/cola', buyOne);
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
