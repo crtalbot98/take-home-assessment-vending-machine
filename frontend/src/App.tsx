3d2072ea99b0e118dfbfa094aebfddc4ea7d97e7
Chandler Talbot <crtalbot98@gmail.com>
2022-06-27 15:08:29 -0400

    Added context for user roles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/main.css';
 import VendingMachine from './components/VendingMachine';
+import { RoleContext, Roles } from './context';
 
 const App: React.FC = () => {
+  const [role, setRole] = useState(Roles.User);
 
-  return <div className='bg-sky-800 grid gap-y-96 p-4 justify-items-center h-screen'>
-    <VendingMachine/>
+  const handleRoleChange = () => {
+    if(role === Roles.Admin) setRole(Roles.User);
+    else setRole(Roles.Admin)
+  }
+
+  return <div className='bg-sky-800 grid gap-y-20 md:gap-y-80 md:p-4 justify-items-center min-h-screen'>
+    <RoleContext.Provider value={{ role }}>
+      <button 
+        onClick={handleRoleChange}
+        className='absolute bg-white p-2 left-0'
+      >
+        Change Role
+      </button>
+      <VendingMachine/>
+    </RoleContext.Provider>
   </div>
 };
 
