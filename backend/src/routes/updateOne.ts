33b6475e1d1b12dd26ab6780b4863090fee6ad8e
Chandler Talbot <50780917+crtalbot98@users.noreply.github.com>
2022-06-27 15:06:26 -0400

    Added body-parser and input validation for updateOne route (#4)

--- backend/src/routes/updateOne.ts
+++ backend/src/routes/updateOne.ts
@@ -1,14 +1,27 @@
 import { Router } from 'express';
 import ColaModel from '../models/colaModel.js';
 
+type ColaUpdates = {
+  cost?: number,
+  num_available?: number
+}
+
 const router = Router();
 
-export default router.get('/updateOne/:id', async (req, res) => {
+export default router.post('/updateOne/:id', async (req, res) => {
   const id = req.params.id;
-  const updates = req.body;
+  const { num_available, cost } = req.body;
+  let updates: ColaUpdates = {};
+
+  if(cost && cost > 0) {
+    updates.cost = cost
+  }
+  else if(num_available && num_available > 0) {
+    updates.num_available = num_available
+  }
 
   try{
-    const updatedCola = ColaModel.findByIdAndUpdate(id, updates);
+    const updatedCola = await ColaModel.findByIdAndUpdate(id, updates);
 
     if(!updatedCola) throw new Error('Unable to find cola');
     res.send(updatedCola)
